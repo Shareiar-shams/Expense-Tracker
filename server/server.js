8051faa68e3fe35b83329a479d4ce502e2c04b83
Shareiar <islamshareiar@gmail.com>
2025-11-16 02:11:15 +0600

    Fixed server js running issue

--- server/server.js
+++ server/server.js
@@ -1,16 +1,22 @@
+require('dotenv').config();
+
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-require('dotenv').config();
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
-// Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+console.log("Mongo URI:", process.env.MONGODB_URI);
 
-// Routes
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => console.log("MongoDB Connected"))
+    .catch(err => console.log("MongoDB Error:", err));
+
 app.get('/', (req, res) => res.send('API is running'));
 
-app.listen(5000, () => console.log('Server running on port 5000'));
+app.listen(process.env.PORT, () =>
+    console.log(`Server running on port ${process.env.PORT}`)
+);
