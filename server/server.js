b7fd9525e3a3c28af94c957d1aa3908d72bb51eb
Shareiar <islamshareiar@gmail.com>
2025-11-16 20:19:35 +0600

    Uncomment transaction routes in server configuration

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const authMiddleware = require("./middleware/auth");
 const authRoutes = require('./routes/authRoutes');
 const categoryRoutes = require('./routes/categoryRoutes');
-// const transactionRoutes = require('./routes/transactionRoutes');
+const transactionRoutes = require('./routes/transactionRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -37,7 +37,7 @@ app.use(authMiddleware);
 app.use('/api/categories', categoryRoutes);
 
 // Transaction routes
-// app.use('/api/transactions', transactionRoutes);
+app.use('/api/transactions', transactionRoutes);
 
 // Start the server
 app.listen(PORT, () => {
