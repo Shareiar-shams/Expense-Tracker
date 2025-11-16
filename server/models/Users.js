6b7d3655f7fc4440a416cc0ce8fc9e986679cdac
Shareiar <islamshareiar@gmail.com>
2025-11-16 19:19:26 +0600

    Refactor user model to use bcryptjs and update password field naming

--- server/models/Users.js
+++ server/models/Users.js
@@ -1,15 +1,15 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
+  username: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  passwordHash: { type: String, required: true },
+  password: { type: String, required: true },
 }, { timestamps: true });
 
 // Optional: method to compare passwords
 userSchema.methods.comparePassword = function (password) {
-  return bcrypt.compare(password, this.passwordHash);
+  return bcrypt.compare(password, this.password);
 };
 
 module.exports = mongoose.model('Users', userSchema);
