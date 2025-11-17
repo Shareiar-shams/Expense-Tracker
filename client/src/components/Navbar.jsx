195cefbc18e7861ebc14e96dddb6d929e3078f00
Shareiar <islamshareiar@gmail.com>
2025-11-18 03:51:49 +0600

    Fix logout redirect and improve tooltip positioning in navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -13,7 +13,7 @@ export default function Navbar() {
 
   const handleLogout = () => {
     logout();
-    navigate('/');
+    navigate('/login');
     setIsUserMenuOpen(false);
   };
 
@@ -59,7 +59,7 @@ export default function Navbar() {
                   }`}
                 >
                   <div>{item.label}</div>
-                  <div className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 px-2 py-1 bg-gray-800 text-white text-xs rounded opacity-0 group-hover:opacity-100 transition-opacity duration-200 pointer-events-none whitespace-nowrap">
+                  <div className="absolute top-full left-1/2 transform -translate-x-1/2 mt-2 px-2 py-1 bg-gray-900 text-white text-xs rounded-lg shadow-lg opacity-0 group-hover:opacity-100 transition-opacity duration-200 pointer-events-none whitespace-nowrap">
                     {item.description}
                   </div>
                 </Link>
