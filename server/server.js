89c2ac2b4488ea94982d310b6e29002d449b4328
Shareiar <islamshareiar@gmail.com>
2025-12-17 00:25:34 +0600

    Simplify CORS middleware configuration

--- server/server.js
+++ server/server.js
@@ -9,19 +9,10 @@ const categoryRoutes = require('./routes/categoryRoutes');
 const transactionRoutes = require('./routes/transactionRoutes');
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-/* =========================
-   Middleware
-========================= */
-app.use(cors({
-  origin: process.env.CLIENT_URL
-    ? process.env.CLIENT_URL.split(',')
-    : '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
-}));
-
-app.options('*', cors());
+// Middleware
+app.use(cors());
 app.use(express.json());
 
 /* =========================
